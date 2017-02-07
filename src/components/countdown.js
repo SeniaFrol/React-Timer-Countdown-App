fb0e1d39a5542a90f6ddae896167c21e9126d3ca
Senia <senia.frolov@gmail.com>
2017-02-07 14:19:02 +0300

    Update status to stop count when it reaches 0

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -27,13 +27,21 @@ class Countdown extends Component {
       }
     }
   }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  }
 
   startTimer() {
     this.timer = setInterval(() => {
-      let newCount = this.state.count - 1;
+      const newCount = this.state.count - 1;
       this.setState({
         count: newCount >=0 ? newCount : 0
       });
+
+      if(newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   }
 
